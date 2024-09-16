7673fb029a0d3aa00c7f730f3f62db4468da3146
Topi-Workspace <tuanworkspace@gmail.com>
2024-09-16 10:39:00 +0700

    employee feature

---
+++ RestaurantManagement.Application/Features/EmployeeFeature/GetAllEmployee/GetAllEmployeeQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using RestaurantManagement.Domain.Entities;
+using RestaurantManagement.Domain.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Features.EmployeeFeature.GetAllEmployee
+{
+    public record GetAllEmployeeQuery(): IRequest<Result<IEnumerable<Employee>>>;
+    
+}
---
+++ RestaurantManagement.Application/Features/EmployeeFeature/UpdateEmployeeForStatus/UpdateEmployeeForStatusHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using RestaurantManagement.Application.Features.EmployeeFeature.UpdateEmployee;
+using RestaurantManagement.Domain.IRepos;
+using RestaurantManagement.Domain.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Features.EmployeeFeature.UpdateEmployeeForStatus
+{
+    public class UpdateEmployeeForStatusHandler : IRequestHandler<UpdateEmployeeForStatusCommand, Result<bool>>
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public UpdateEmployeeForStatusHandler(IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork)
+        {
+            _employeeRepository = employeeRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Result<bool>> Handle(UpdateEmployeeForStatusCommand request, CancellationToken cancellationToken)
+        {
+            var result = new Result<bool>
+            {
+                ResultValue = false,
+                IsSuccess = false,
+                Errors = new List<string>()
+            };
+            
+
+            return result;
+        }
+    }
+}
