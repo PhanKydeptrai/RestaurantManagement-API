925d0131b1ddc6ffa63326701f6d716653413dd6
PhanKydeptrai <kyp194490@gmail.com>
2024-10-30 22:23:27 +0700

    Fix DeleteTableCommandValidator logic for table status checks

--- RestaurantManagement.Application/Features/TableFeature/Commands/DeleteTable/DeleteTableCommandValidator.cs
+++ RestaurantManagement.Application/Features/TableFeature/Commands/DeleteTable/DeleteTableCommandValidator.cs
@@ -14,9 +14,9 @@ public class DeleteTableCommandValidator : AbstractValidator<DeleteTableCommand>
             .WithMessage("Table id is required")
             .Must(a => tableRepository.IsTableExist(a).Result == true)
             .WithMessage("Table is not exist") //Kiểm tra bàn có tồn tại hay không?
-            .Must(a => tableRepository.GetTableStatus(a).Result == "Empty")
+            .Must(a => tableRepository.GetActiveStatus(a).Result == "Empty")
             .WithMessage("Table is not empty")
-            .Must(a => tableRepository.GetActiveStatus(a).Result == "Active")
+            .Must(a => tableRepository.GetTableStatus(a).Result == "Active")
             .WithMessage("Table is still InActive");//Kiểm tra bàn có trạng thái là empty hay không?
     }
 }
