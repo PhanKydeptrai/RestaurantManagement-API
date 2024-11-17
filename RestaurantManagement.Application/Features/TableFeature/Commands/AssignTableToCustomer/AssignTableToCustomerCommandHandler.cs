88f5b9252be337a1537483cab709a8ca43e39943
PhanKydeptrai <kyp194490@gmail.com>
2024-11-17 22:19:33 +0700

    refactor: clean up code and add stable version regions in booking and table assignment features

--- RestaurantManagement.Application/Features/TableFeature/Commands/AssignTableToCustomer/AssignTableToCustomerCommandHandler.cs
+++ RestaurantManagement.Application/Features/TableFeature/Commands/AssignTableToCustomer/AssignTableToCustomerCommandHandler.cs
@@ -1,17 +1,20 @@
 using RestaurantManagement.Application.Abtractions;
+using RestaurantManagement.Application.Data;
 using RestaurantManagement.Application.Extentions;
 using RestaurantManagement.Domain.IRepos;
 using RestaurantManagement.Domain.Shared;
 
 namespace RestaurantManagement.Application.Features.TableFeature.Commands.AssignTableToCustomer;
 
+
+
+#region Stable version
 public class GetTableForCustomerCommandHandler(
     ITableRepository tableRepository,
     IUnitOfWork unitOfWork) : ICommandHandler<AssignTableToCustomerCommand>
 {
     public async Task<Result> Handle(AssignTableToCustomerCommand request, CancellationToken cancellationToken)
     {
-        
         //Validate request
         var validator = new AssignTableToCustomerCommandValidator(tableRepository);
         Error[]? errors = null;
@@ -20,9 +23,35 @@ public class GetTableForCustomerCommandHandler(
         {
             return Result.Failure(errors!);
         }
-        
         await tableRepository.UpdateActiveStatus(int.Parse(request.id), "Occupied");
         await unitOfWork.SaveChangesAsync();
         return Result.Success();
     }
 }
+#endregion
+
+#region In development
+// public class GetTableForCustomerCommandHandler(
+//     IApplicationDbContext context,
+//     ITableRepository tableRepository,
+//     IUnitOfWork unitOfWork) : ICommandHandler<AssignTableToCustomerCommand>
+// {
+//     public async Task<Result> Handle(AssignTableToCustomerCommand request, CancellationToken cancellationToken)
+//     {
+        
+//         //Validate request
+//         var validator = new AssignTableToCustomerCommandValidator(tableRepository, context);
+
+//         Error[]? errors = null;
+//         var isValid = await Task.Run(() => ValidateRequest.RequestValidator(validator, request, out errors));
+//         if (!isValid)
+//         {
+//             return Result.Failure(errors!);
+//         }
+        
+//         await tableRepository.UpdateActiveStatus(int.Parse(request.id), "Occupied");
+//         await unitOfWork.SaveChangesAsync();
+//         return Result.Success();
+//     }
+// }
+#endregion
--- RestaurantManagement.Application/Features/TableFeature/Commands/AssignTableToCustomer/AssignTableToCustomerCommandValidator.cs
+++ RestaurantManagement.Application/Features/TableFeature/Commands/AssignTableToCustomer/AssignTableToCustomerCommandValidator.cs
@@ -1,13 +1,15 @@
 using FluentValidation;
+using RestaurantManagement.Application.Data;
 using RestaurantManagement.Domain.IRepos;
 
 namespace RestaurantManagement.Application.Features.TableFeature.Commands.AssignTableToCustomer;
 
+#region Stable version
 public class AssignTableToCustomerCommandValidator : AbstractValidator<AssignTableToCustomerCommand>
 {
     public AssignTableToCustomerCommandValidator(ITableRepository tableRepository)
     {
-        RuleFor(p => p.id)            
+        RuleFor(p => p.id)
             .Must(a => tableRepository.GetActiveStatus(int.Parse(a)).Result == "Empty")
             .WithMessage("This table is not empty.")
             .When(a => int.TryParse(a.id, out _))
@@ -18,6 +20,47 @@ public class AssignTableToCustomerCommandValidator : AbstractValidator<AssignTab
             .WithMessage("{PropertyName} is required.")
             .Must(a => int.TryParse(a, out _))
             .WithMessage("{PropertyName} must be a number.");
-        
+
     }
 }
+#endregion
+
+
+#region In development
+
+// public AssignTableToCustomerCommandValidator(ITableRepository tableRepository, IApplicationDbContext dbcontext)
+//     {
+//         RuleFor(p => p.id)
+//             .Custom(async (tableId, context) =>
+//             {
+//                 var tableStatus = await tableRepository.GetActiveStatus(int.Parse(tableId));
+//                 if (tableStatus != "Empty" && tableStatus != "Booked")
+//                 {
+//                     TimeOnly timeOnlyNow = TimeOnly.FromDateTime(DateTime.Now.AddHours(-3));
+//                     DateOnly dateOnlyNow = DateOnly.FromDateTime(DateTime.Now);
+//                     var check = await dbcontext.Tables
+//                         .Where(a => a.TableId == int.Parse(tableId))
+//                         .Include(a => a.BookingDetails)
+//                         .ThenInclude(a => a.Booking)
+//                         .Select(a => a.BookingDetails
+//                         .Where(a => a.Booking.BookingStatus == "Booked" 
+//                         && a.Booking.BookingTime > timeOnlyNow 
+//                         && a.Booking.BookingDate == dateOnlyNow))
+//                         .AnyAsync();
+//                     if(check == true)
+//                     {
+//                         context.AddFailure("This table is not empty.");
+//                     }
+//                 }
+//             })
+//             .When(a => int.TryParse(a.id, out _))
+//             .NotNull()
+//             .WithMessage("{PropertyName} is required.")
+//             .NotEmpty()
+//             .WithMessage("{PropertyName} is required.")
+//             .Must(a => int.TryParse(a, out _))
+//             .WithMessage("{PropertyName} must be a number.");
+
+//     }
+// }
+#endregion
