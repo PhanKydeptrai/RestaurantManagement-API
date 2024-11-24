1d77e3c67fb8ac5e4a5157951356097b3aa6f657
PhanKydeptrai <kyp194490@gmail.com>
2024-11-24 21:57:12 +0700

    feat: enhance voucher application logic with discount type handling and cleanup

--- RestaurantManagement.Application/Features/OrderFeature/Commands/ApplyVoucher/ApplyVoucherCommandHandler.cs
+++ RestaurantManagement.Application/Features/OrderFeature/Commands/ApplyVoucher/ApplyVoucherCommandHandler.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using Microsoft.EntityFrameworkCore;
 using RestaurantManagement.Application.Abtractions;
 using RestaurantManagement.Application.Data;
@@ -42,7 +41,6 @@ public class ApplyVoucherCommandHandler : ICommandHandler<ApplyVoucherCommand>
             return Result.Failure(errors!);
         }
 
-        //Apply voucher 
         //Kiểm tra xem bàn đã có order chưa
 
         var order = await _context.Tables
@@ -57,7 +55,12 @@ public class ApplyVoucherCommandHandler : ICommandHandler<ApplyVoucherCommand>
             return Result.Failure(error);
         }
 
-
+        /*
+        * try catch để kiểm tra xem order đã có bill chưa
+        * Cần kiểm tra vì nếu khách có đặt bàn trước thì bill đã có sẵn khi thanh toán booking
+        * Nếu có thì cập nhật lại bill
+        * Nếu chưa thì tạo mới bill
+        */
         try
         {
             Bill? bill = await _context.Bills // Tìm bill id theo order id
@@ -81,7 +84,23 @@ public class ApplyVoucherCommandHandler : ICommandHandler<ApplyVoucherCommand>
             if (isCustomerHasThisVoucher != null && isCustomerHasThisVoucher.Quantity > 0 /*Khách hàng có voucher*/
                 && voucher.MinimumOrderAmount <= bill.Total /*Tổng tiền hóa đơn lớn hơn điều kiện voucher*/)
             {
-                bill.Total -= voucher.MaximumDiscountAmount;
+                if (voucher.VoucherType == "DirectDiscount")
+                {
+                    bill.Total -= voucher.MaximumDiscountAmount;
+                }
+                else
+                {
+                    decimal discountAmount = bill.Total * (decimal)voucher.PercentageDiscount / 100;
+                    if(discountAmount > voucher.MaximumDiscountAmount)
+                    {
+                        bill.Total -= voucher.MaximumDiscountAmount;
+                    }
+                    else
+                    {
+                        bill.Total -= discountAmount;
+                    }
+                }
+
                 bill.VoucherId = voucher.VoucherId;
                 isCustomerHasThisVoucher.Quantity -= 1; //Giảm số lượng voucher của khách hàng
             }
@@ -121,7 +140,22 @@ public class ApplyVoucherCommandHandler : ICommandHandler<ApplyVoucherCommand>
             if (isCustomerHasThisVoucher != null && isCustomerHasThisVoucher.Quantity > 0 /*Khách hàng có voucher*/
                 && voucher.MinimumOrderAmount <= bill.Total /*Tổng tiền hóa đơn lớn hơn điều kiện voucher*/)
             {
-                bill.Total -= voucher.MaximumDiscountAmount;
+                if (voucher.VoucherType == "DirectDiscount")
+                {
+                    bill.Total -= voucher.MaximumDiscountAmount;
+                }
+                else
+                {
+                    decimal discountAmount = bill.Total * (decimal)voucher.PercentageDiscount / 100;
+                    if(discountAmount > voucher.MaximumDiscountAmount)
+                    {
+                        bill.Total -= voucher.MaximumDiscountAmount;
+                    }
+                    else
+                    {
+                        bill.Total -= discountAmount;
+                    }
+                }
                 bill.VoucherId = voucher.VoucherId;
                 isCustomerHasThisVoucher.Quantity -= 1; //Giảm số lượng voucher của khách hàng
             }
@@ -138,150 +172,3 @@ public class ApplyVoucherCommandHandler : ICommandHandler<ApplyVoucherCommand>
         return Result.Success();
     }
 }
-
-
-#region Lmao
-// using System.Runtime.CompilerServices;
-// using Microsoft.EntityFrameworkCore;
-// using RestaurantManagement.Application.Abtractions;
-// using RestaurantManagement.Application.Data;
-// using RestaurantManagement.Application.Extentions;
-// using RestaurantManagement.Domain.Entities;
-// using RestaurantManagement.Domain.IRepos;
-// using RestaurantManagement.Domain.Shared;
-
-// namespace RestaurantManagement.Application.Features.OrderFeature.Commands.ApplyVoucher;
-
-// public class ApplyVoucherCommandHandler : ICommandHandler<ApplyVoucherCommand>
-// {
-//     private readonly IApplicationDbContext _context;
-//     private readonly ICustomerRepository _customerRepository;
-//     private readonly IVoucherRepository _voucherRepository;
-//     private readonly ITableRepository _tableRepository;
-//     private readonly IUnitOfWork _unitOfWork;
-
-//     public ApplyVoucherCommandHandler(
-//         IApplicationDbContext context,
-//         IUnitOfWork unitOfWork,
-//         ICustomerRepository customerRepository,
-//         IVoucherRepository voucherRepository,
-//         ITableRepository tableRepository)
-//     {
-//         _context = context;
-//         _unitOfWork = unitOfWork;
-//         _customerRepository = customerRepository;
-//         _voucherRepository = voucherRepository;
-//         _tableRepository = tableRepository;
-//     }
-
-//     public async Task<Result> Handle(ApplyVoucherCommand request, CancellationToken cancellationToken)
-//     {
-//         //validate
-//         var validator = new ApplyVoucherCommandValidator(_voucherRepository, _customerRepository, _tableRepository);
-//         Error[]? errors = null;
-//         var isValid = await Task.Run(() => ValidateRequest.RequestValidator(validator, request, out errors));
-//         if (!isValid)
-//         {
-//             return Result.Failure(errors!);
-//         }
-
-//         //Apply voucher 
-//         //Kiểm tra xem bàn đã có order chưa
-
-//         var order = await _context.Tables
-//             .Include(a => a.Orders)
-//             .Where(a => a.TableId == int.Parse(request.tableId))
-//             .Select(a => a.Orders.FirstOrDefault(a => a.PaymentStatus == "Unpaid"))
-//             .FirstOrDefaultAsync();
-
-//         if (order == null)
-//         {
-//             var error = new[] { new Error("Order", "Table does not have any order.") };
-//             return Result.Failure(error);
-//         }
-
-
-//         try
-//         {
-//             Bill? bill = await _context.Bills // Tìm bill id theo order id
-//                         .Where(a => a.OrderId == order.OrderId)
-//                         .FirstOrDefaultAsync();
-
-//             bill.CreatedDate = DateTime.Now;
-//             //Kiểm tra khách có nhập voucher hay không
-//             if (!string.IsNullOrEmpty(request.voucherCode))
-//             {
-//                 //Kiểm tra voucher còn thời hạn hay không?
-//                 var voucher = await _context.Vouchers
-//                     .Where(a => a.VoucherName == request.voucherCode && a.ExpiredDate >= DateTime.Now)
-//                     .FirstOrDefaultAsync();
-
-//                 //Kiểm tra khách hàng có voucher này hay không
-//                 var isCustomerHasThisVoucher = await _context.CustomerVouchers
-//                     .Include(a => a.Customer)
-//                     .ThenInclude(a => a.User)
-//                     .Where(a => a.VoucherId == voucher.VoucherId && a.Customer.User.Phone == request.phoneNumber)
-//                     .FirstOrDefaultAsync();
-
-//                 if (isCustomerHasThisVoucher != null /*Khách hàng có voucher*/
-//                     && voucher != null /*Voucher còn hạn*/
-//                     && voucher.MinimumOrderAmount >= bill.Total /*Tổng tiền hóa đơn lớn hơn điều kiện voucher*/)
-//                 {
-//                     bill.Total -= voucher.MaximumDiscountAmount;
-//                     isCustomerHasThisVoucher.Quantity -= 1; //Giảm số lượng voucher của khách hàng
-//                 }
-//             }
-//         }
-//         catch (Exception)
-//         {
-//             //tạo bill
-//             var bill = new Bill
-//             {
-//                 BillId = Ulid.NewUlid(),
-//                 BookId = null,
-//                 CreatedDate = DateTime.Now,
-//                 OrderId = order.OrderId,
-//                 Total = order.Total,
-//                 PaymentStatus = string.Empty,
-//                 PaymentType = string.Empty
-//             };
-
-//             //Kiểm tra khách có nhập voucher hay không
-//             if (!string.IsNullOrEmpty(request.voucherCode))
-//             {
-//                 //Kiểm tra voucher còn thời hạn hay không?
-//                 Voucher? voucher = await _context.Vouchers
-//                     .Where(a => a.VoucherCode == request.voucherCode)
-//                     .FirstOrDefaultAsync();
-
-//                 if (voucher == null)
-//                 {
-//                     var error = new[] { new Error("Order", "Voucher is not found.") };
-//                     return Result.Failure(error);
-//                 }
-
-//                 //Kiểm tra khách hàng có voucher này hay không
-//                 CustomerVoucher? isCustomerHasThisVoucher = await _context.CustomerVouchers
-//                     .Include(a => a.Customer)
-//                     .ThenInclude(a => a.User)
-//                     .Where(a => a.VoucherId == voucher.VoucherId && a.Customer.User.Phone == request.phoneNumber)
-//                     .FirstOrDefaultAsync();
-
-//                 if (isCustomerHasThisVoucher != null /*Khách hàng có voucher*/
-//                     && voucher != null /*Voucher còn hạn*/
-//                     && voucher.MinimumOrderAmount >= bill.Total /*Tổng tiền hóa đơn lớn hơn điều kiện voucher*/)
-//                 {
-//                     bill.Total -= voucher.MaximumDiscountAmount;
-//                     isCustomerHasThisVoucher.Quantity -= 1; //Giảm số lượng voucher của khách hàng
-//                 }
-//             }
-
-//             await _context.Bills.AddAsync(bill);
-//         }
-
-//         await _unitOfWork.SaveChangesAsync();
-//         return Result.Success();
-//     }
-// }
-
-#endregion
