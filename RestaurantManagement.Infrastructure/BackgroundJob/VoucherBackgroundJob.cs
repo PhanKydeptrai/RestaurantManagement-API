b917f2e3c5fd14320431b4b5cd0d9a6189a4efe9
PhanKydeptrai <kyp194490@gmail.com>
2024-12-16 20:55:44 +0700

    Implement email notification for expired vouchers and configure SMTP settings

--- RestaurantManagement.Infrastructure/BackgroundJob/VoucherBackgroundJob.cs
+++ RestaurantManagement.Infrastructure/BackgroundJob/VoucherBackgroundJob.cs
@@ -1,4 +1,8 @@
+using System.Net;
+using System.Net.Mail;
+using FluentEmail.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Quartz;
 using RestaurantManagement.Application.Data;
 using RestaurantManagement.Domain.Entities;
@@ -9,14 +13,20 @@ namespace RestaurantManagement.Infrastructure.BackgroundJob;
 public class VoucherBackgroundJob : IJob
 {
     private readonly IApplicationDbContext _context;
+    private readonly IConfiguration _configuration;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IFluentEmail _fluentEmail;
 
     public VoucherBackgroundJob(
-        IApplicationDbContext context, 
-        IUnitOfWork unitOfWork)
+        IApplicationDbContext context,
+        IUnitOfWork unitOfWork,
+        IFluentEmail fluentEmail,
+        IConfiguration configuration)
     {
         _context = context;
         _unitOfWork = unitOfWork;
+        _fluentEmail = fluentEmail;
+        _configuration = configuration;
     }
 
     public async Task Execute(IJobExecutionContext context)
@@ -32,12 +42,94 @@ public class VoucherBackgroundJob : IJob
         foreach (var voucher in expiredVouchers)
         {
             voucher.Status = "InActive";
+
+            CustomerVoucher[]? customerVouchers = await _context.CustomerVouchers
+                .Include(a => a.Voucher)
+                .Include(a => a.Customer)
+                .ThenInclude(a => a.User)
+                .Where(a => a.VoucherId == voucher.VoucherId)
+                .ToArrayAsync();
+            foreach (var customerVoucher in customerVouchers)
+            {
+                //Gửi mail thông báo cho khách hàng
+                bool emailSent = false;
+                int retryCount = 0;
+                int maxRetries = 5;
+
+                do
+                {
+                    if (_configuration["Environment"] == "Development")
+                    {
+                        try
+                        {
+                            await _fluentEmail.To(customerVoucher.Customer.User.Email)
+                                .Subject("Nhà hàng Nhum nhum - Voucher hết hạn 😢")
+                                .Body($"Nhà hàng Nhum Nhum xin thông báo <br> Voucher {customerVoucher.Voucher.VoucherCode} của bạn đã hết hạn.", isHtml: true)
+                                .SendAsync();
+
+                            emailSent = true;
+                        }
+                        catch
+                        {
+                            retryCount++;
+                            // if (retryCount >= maxRetries)
+                            // {
+                            //     return Result.Failure(new[] { new Error("Email", "Failed to send email") });
+                            // }
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            #region Send Email using Gmail SMTP
+                            // Thông tin đăng nhập và cài đặt máy chủ SMTP
+                            string fromEmail = "nhumnhumrestaurant@gmail.com"; // Địa chỉ Gmail của bạn
+                            string toEmail = customerVoucher.Customer.User.Email;  // Địa chỉ người nhận
+                            string password = "ekgh lntd brrv bdyj";   // Mật khẩu ứng dụng (nếu bật 2FA) hoặc mật khẩu của tài khoản Gmail
+
+                            var smtpClient = new SmtpClient("smtp.gmail.com")
+                            {
+                                Port = 587, // Cổng sử dụng cho TLS
+                                Credentials = new NetworkCredential(fromEmail, password), // Đăng nhập vào Gmail
+                                EnableSsl = true // Kích hoạt SSL/TLS
+                            };
+
+                            var mailMessage = new MailMessage
+                            {
+                                From = new MailAddress(fromEmail),
+                                Subject = "Nhà hàng Nhum nhum - Voucher hết hạn 😢",
+                                Body = $"", // Nội dung email
+                                IsBodyHtml = true // Nếu muốn gửi email ở định dạng HTML
+                            };
+
+                            mailMessage.To.Add(toEmail);
+
+                            // Gửi email
+                            smtpClient.Send(mailMessage);
+                            #endregion
+
+                            emailSent = true;
+                        }
+                        catch
+                        {
+                            retryCount++;
+                            // if (retryCount >= maxRetries)
+                            // {
+                            //     return Result.Failure(new[] { new Error("Email", "Failed to send email") });
+                            // }
+                        }
+                    }
+
+                }
+                while (!emailSent && retryCount < maxRetries);
+            }
         }
 
-        //Cập nhật thay đổi cho voucher
+        //Cập nhật thay đổi cho voucher        
 
         await _unitOfWork.SaveChangesAsync();
 
-        Console.WriteLine("Voucher background job is running");        
+        Console.WriteLine("Voucher background job is running");
     }
 }
