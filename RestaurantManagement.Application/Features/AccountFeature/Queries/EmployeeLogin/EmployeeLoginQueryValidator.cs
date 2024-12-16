9af3bbd4d0c5f0fc6c0cd788ffa1c880186f0a6a
PhanKydeptrai <kyp194490@gmail.com>
2024-12-16 21:22:01 +0700

    Refactor login string validation to simplify email and phone number checks

--- RestaurantManagement.Application/Features/AccountFeature/Queries/EmployeeLogin/EmployeeLoginQueryValidator.cs
+++ RestaurantManagement.Application/Features/AccountFeature/Queries/EmployeeLogin/EmployeeLoginQueryValidator.cs
@@ -6,30 +6,36 @@ public class EmployeeLoginQueryValidator : AbstractValidator<EmployeeLoginQuery>
 {
     public EmployeeLoginQueryValidator()
     {
-        RuleFor(a => a.loginString)
-            .NotEmpty().WithMessage("Phone number or Email is required.")
-            .NotNull().WithMessage("Phone number or Email is required.")
-            .Custom((loginString, context) =>
-            {
-                if (loginString.Any(char.IsLetter))
-                {
-                    var emailValidator = new InlineValidator<string>();
-                    emailValidator.RuleFor(email => email).EmailAddress().WithMessage("Email is invalid.");
+        // RuleFor(a => a.loginString)
+        //     .NotEmpty().WithMessage("Phone number or Email is required.")
+        //     .NotNull().WithMessage("Phone number or Email is required.")
+        //     .Custom((loginString, context) =>
+        //     {
+        //         if (loginString.Any(char.IsLetter))
+        //         {
+        //             var emailValidator = new InlineValidator<string>();
+        //             emailValidator.RuleFor(email => email).EmailAddress().WithMessage("Email is invalid.");
 
-                    var validationResult = emailValidator.Validate(loginString);
-                    if (!validationResult.IsValid)
-                    {
-                        context.AddFailure("Email is invalid.");
-                    }
-                }
-                else
-                {
-                    if (!System.Text.RegularExpressions.Regex.IsMatch(loginString, @"^0\d{9}$"))
-                    {
-                        context.AddFailure("PhoneNumber must start with 0 and be 10 digits long.");
-                    }
-                }
-            });
+        //             var validationResult = emailValidator.Validate(loginString);
+        //             if (!validationResult.IsValid)
+        //             {
+        //                 context.AddFailure("Email is invalid.");
+        //             }
+        //         }
+        //         else
+        //         {
+        //             if (!System.Text.RegularExpressions.Regex.IsMatch(loginString, @"^0\d{9}$"))
+        //             {
+        //                 context.AddFailure("PhoneNumber must start with 0 and be 10 digits long.");
+        //             }
+        //         }
+        //     });
+
+        RuleFor(a => a.loginString)
+            .NotNull().WithMessage("Email is required.")
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Email is invalid.");
+            
 
         RuleFor(a => a.passWord)
             .NotEmpty().NotNull()
--- RestaurantManagement.Application/Features/AccountFeature/Queries/Login/LoginQueryValidator.cs
+++ RestaurantManagement.Application/Features/AccountFeature/Queries/Login/LoginQueryValidator.cs
@@ -6,30 +6,39 @@ public class LoginQueryValidator : AbstractValidator<LoginQuery>
 {
     public LoginQueryValidator()
     {
-        RuleFor(a => a.loginString)
-            .NotNull().WithMessage("Phone number or Email is required.")
-            .NotEmpty().WithMessage("Phone number or Email is required.")
-            .Custom((loginString, context) =>
-            {
-                if (loginString.Any(char.IsLetter))
-                {
-                    var emailValidator = new InlineValidator<string>();
-                    emailValidator.RuleFor(email => email).EmailAddress().WithMessage("Email is invalid.");
+        // RuleFor(a => a.loginString)
+        //     .NotNull().WithMessage("Phone number or Email is required.")
+        //     .NotEmpty().WithMessage("Phone number or Email is required.")
+        //     .Custom((loginString, context) =>
+        //     {
+        //         if (loginString.Any(char.IsLetter))
+        //         {
+        //             var emailValidator = new InlineValidator<string>();
+        //             emailValidator.RuleFor(email => email).EmailAddress().WithMessage("Email is invalid.");
 
-                    var validationResult = emailValidator.Validate(loginString);
-                    if (!validationResult.IsValid)
-                    {
-                        context.AddFailure("Email is invalid.");
-                    }
-                }
-                else
-                {
-                    if (!System.Text.RegularExpressions.Regex.IsMatch(loginString, @"^0\d{9}$"))
-                    {
-                        context.AddFailure("PhoneNumber must start with 0 and be 10 digits long.");
-                    }
-                }
-            }).When(a => !string.IsNullOrEmpty(a.loginString));
+        //             var validationResult = emailValidator.Validate(loginString);
+        //             if (!validationResult.IsValid)
+        //             {
+        //                 context.AddFailure("Email is invalid.");
+        //             }
+        //         }
+        //         else
+        //         {
+        //             if (!System.Text.RegularExpressions.Regex.IsMatch(loginString, @"^0\d{9}$"))
+        //             {
+        //                 context.AddFailure("PhoneNumber must start with 0 and be 10 digits long.");
+        //             }
+        //         }
+        //     }).When(a => !string.IsNullOrEmpty(a.loginString));
+
+        RuleFor(a => a.loginString)
+            .NotNull()
+            .WithMessage("Email is required.")
+            .NotEmpty()
+            .WithMessage("Email is required.")
+            .EmailAddress()
+            .WithMessage("Email is invalid.");
+            
 
         RuleFor(a => a.passWord)
             .NotNull()
